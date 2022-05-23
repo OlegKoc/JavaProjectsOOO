162b43bbb1ec0d174b48bcc2225f3a988ccfdeb0
OlegKoc <koralll6@yandex.ru>
2022-05-23 20:05:37 +0300

    init2

--- src/Solution.java
+++ src/Solution.java
@@ -2,5 +2,6 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("здорова");
         System.out.println();
+        System.out.println("ахахахаха");
     }
 }
