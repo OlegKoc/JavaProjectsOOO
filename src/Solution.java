5c02b2e35130fdade397f8c2b86cbdede292e396
OlegKoc <105979046+OlegKoc@users.noreply.github.com>
2022-05-25 20:21:30 +0300

    Delete Solution.java

--- src/Solution.java
+++
@@ -1,7 +0,0 @@
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println("здорова");
-        System.out.println();
-        System.out.println("ахахахаха");
-    }
-}
